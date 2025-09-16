3de8b5d8e84f987d4abcd41389af2ab95ac296a7
AlexGebrah <alexisgebrah@gmail.com>
2025-09-16 18:39:53 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-
-import './App.css'
-
-function App() {
-
-
-  return (
-    <>
-
-
-    </>
-  )
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { Dashboard } from './pages/Dashboard.tsx'
+export function App() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    )
 }
-
-export default App
