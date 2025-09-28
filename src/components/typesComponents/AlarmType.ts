581d48ddf1a0fa8280cdf69c88ea40836bba4e08
AlexGebrah <alexisgebrah@gmail.com>
2025-09-28 21:55:56 +0300

    with Creat Project Materials Reports Alarm

---
+++ src/components/typesComponents/AlarmType.ts
@@ -0,0 +1,18 @@
+import type {ProjectKind, ProjectType} from "./ProjectType.ts";
+import type {UserType} from "./UserType.ts";
+
+export type AlarmType =
+    {
+        id: string,
+        project: {
+            id: ProjectType<ProjectKind>["id"],
+            projectName: ProjectType<ProjectKind>["projectName"],
+        },
+        user: {
+            id: UserType["id"],
+            fullName: UserType["fullName"],
+        },
+        dateCreate: string,
+        status: "draft" | "active" | "done",
+        description: string,
+    }
